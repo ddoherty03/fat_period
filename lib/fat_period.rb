23842fa1b47e7d7c6a52c57904027c9e48466631
Daniel E. Doherty <ded@ddoherty.net>
2025-10-22 06:33:38 -0500

    Require fat_date dependency; fat_core only version >= 6.0; remove require fat_core/date.

--- lib/fat_period.rb
+++ lib/fat_period.rb
@@ -1,9 +1,12 @@
 require 'date'
 
+require 'active_support'
+
 require 'fat_period/version'
 require 'fat_period/date'
 require 'fat_period/period'
 
-require 'fat_core/date'
 require 'fat_core/range'
 require 'fat_core/string'
+
+require 'fat_date'
